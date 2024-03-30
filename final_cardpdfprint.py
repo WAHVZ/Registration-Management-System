5314f8dd18f2440fb9ce1ea16bc4b934f825efe0
Wali Ashraf <108512201+WAHVZ@users.noreply.github.com>
2024-03-31 02:03:37 +0500

    Update final_cardpdfprint.py

--- final_cardpdfprint.py
+++ final_cardpdfprint.py
@@ -5,7 +5,7 @@ import win32api
 
 def generate_badge(name, designation, company, name_x, name_y, desig_x, desig_y, comp_x, comp_y):
     filename = name + "_badge.pdf"
-    c = canvas.Canvas(filename, pagesize=(4*inch, 5.5*inch))
+    c = canvas.Canvas(filename, pagesize=(4*inch, 5.5*inch))          #Size of the page is set to 4 * 5.5. Change it according to your needs.
 
     # Set font properties
     c.setFont("Times-Bold", 25)
@@ -23,6 +23,8 @@ def generate_badge(name, designation, company, name_x, name_y, desig_x, desig_y,
 
 if __name__ == "__main__":
     # Get inputs from user
+    # Take inputs as you please
+    
     '''name = input("Enter your name (for filename): ")
     designation = input("Enter your designation: ")
     company = input("Enter your company: ")'''
@@ -30,7 +32,9 @@ if __name__ == "__main__":
     designation = "Student"
     company = "NED"
 
-    # Set the exact location of entries
+    # Manaully set the exact location of entries                               
+    # Increasing the x values will move the variable to right hand side and y values to the left. Same goes to y ones.
+    
     name_x = 1.25 * inch  # Adjust as needed
     name_y = 3.25 * inch  # Adjust as needed
     desig_x = 1 * inch  # Adjust as needed
